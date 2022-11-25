4e777b3cb6b8f0c92ed0276d527145e9a74ffa3b
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-25 12:26:18 +0530

    add todo page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { RouterModule, Routes, ROUTES } from '@angular/router';
+const  myRoute : Routes= [
+  {
+    path :"",
+    component: AddTodoComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
