3c5a63e1cf4fb16e46ba930a99888293f0e8b9a4
krishnanunnijp <krishnanunni.j@nestgroup.net>
2022-11-29 14:11:29 +0530

    readed data from the website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { RouterModule, Routes, ROUTES } from '@angular/router';
 import { ViewTodoComponent } from './view-todo/view-todo.component';
 import { TodoNavbarComponent } from './todo-navbar/todo-navbar.component';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http'
 const  myRoute : Routes= [
   {
     path :"",
@@ -30,7 +31,8 @@ const  myRoute : Routes= [
     BrowserModule,
     AppRoutingModule,
     RouterModule.forRoot(myRoute),
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
